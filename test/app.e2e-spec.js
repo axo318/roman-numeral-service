8ebbdf8ad3def86ac6546718ec78b0a034d45310
Vangelis <axokid318@gmail.com>
2022-06-11 18:49:03 +0200

    add e2e tests

--- test/app.e2e-spec.js
+++ test/app.e2e-spec.js
@@ -14,10 +14,52 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('should respond with correct Roman numeral', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/romannumeral?query=137')
       .expect(200)
-      .expect('hello');
+      .expect('CXXXVII');
+  });
+
+  it('should fail because input is not numeric', () => {
+    return request(app.getHttpServer())
+      .get('/romannumeral?query=1a')
+      .expect(400)
+      .expect({
+        'statusCode':400,
+        'message':'Validation failed (numeric string is expected)',
+        'error':'Bad Request'
+      });
+  });
+
+  it('should fail because input is a decimal', () => {
+    return request(app.getHttpServer())
+      .get('/romannumeral?query=1.1')
+      .expect(400)
+      .expect({
+        'statusCode':400,
+        'message':'Validation failed (numeric string is expected)',
+        'error':'Bad Request'
+      });
+  });
+
+  it('should fail because input is less than 0', () => {
+    return request(app.getHttpServer())
+      .get('/romannumeral?query=-1')
+      .expect(400)
+      .expect({
+        'statusCode':400,
+        'message':'Number must be greater or equal to 0'
+      });
+  });
+
+  it('should fail because input is too big', () => {
+    return request(app.getHttpServer())
+      .get('/romannumeral?query=4000000000')
+      .expect(400)
+      .expect({
+        'statusCode':400,
+        'message':'Number must be smaller or equal to 3999999999'
+      });
   });
 });
